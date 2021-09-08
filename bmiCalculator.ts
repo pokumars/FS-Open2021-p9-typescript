5a4b3029573a4e4c9bb699b30e453d12f2e09b5d
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-09-08 05:51:18 +0300

    Ex 9.3 add argumentParser for bmiCalculator so it can bne triggered from cli

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,17 +1,46 @@
-const calculateBmi = (height:number, mass:number): string => {
+const calculateBmi = (height: number, mass: number): string => {
 
-  const heightInMetres: number = height/100
-  const bmi = (mass/(Math.pow(heightInMetres, 2)))
+  const heightInMetres: number = height / 100
+  const bmi = (mass / (Math.pow(heightInMetres, 2)))
 
   if (bmi < 25) {
     return `Healthy. Your BMI result is ${bmi}`;
-  } 
-  else if (bmi >= 25 || bmi <30){
+  }
+  else if (bmi >= 25 || bmi < 30) {
     return `Overweight. Your BMI result is ${bmi}`;
-  } 
-  else if(bmi >= 30) {
+  }
+  else if (bmi >= 30) {
     return `Obese. Your BMI result is ${bmi}.`;
   }
 }
 
-console.log(calculateBmi(180, 74))
+interface BmiParams {
+  height: number
+  mass: number
+}
+
+const parseArguments = (args: Array<string>): BmiParams => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length < 4) throw new Error("Too many arguments");
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+
+    return {
+      height: Number(args[2]),
+      mass: Number(args[3])
+    }
+  } else {
+    throw new Error("Some of the expected numbers were not numbers");
+
+  }
+}
+
+
+
+try {
+  const { mass, height } = parseArguments(process.argv);
+
+  console.log(calculateBmi(height, mass))
+} catch (e) {
+  console.log('Error, something bad happened, message: ', e.message);
+}
