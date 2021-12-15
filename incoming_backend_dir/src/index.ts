10b95f5e36c39afc54281eb0554f419a7e9f6367
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-12-16 00:49:20 +0200

    uibuild

--- incoming_backend_dir/src/index.ts
+++ incoming_backend_dir/src/index.ts
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
+app.use(express.static('build'));
 app.use('/api/diagnoses', diagnosesRouter);
 app.use('/api/patients', patientsRouter);
 
